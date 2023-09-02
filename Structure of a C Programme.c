4618f62d9ee75e4697f9dd688b4ae469a0fbc9e6
Anurag Kumar <140825596+Anuragji1@users.noreply.github.com>
2023-09-02 23:06:01 +0530

    Add files via upload
    In this code, I explained in every single line of a Structure of C Programme and why i use.

---
+++ Structure of a C Programme.c
@@ -0,0 +1,14 @@
+#include<stdio.h> // here, h =header file, stdio.h = Standard input/output 
+#include<conio.h> // Here, conio.h = Consol input/output function
+
+void main() //main function
+{ clrscr;      // clrscr used for cleans the screen 
+printf("Welcome to the first class of programming in c.\n this subject contains 6 units"); //Here, \n = it is used for next line appearing statement
+// printf = It is the output function to display anything
+// Anything we write in the double core that is known as string.
+// After function Void main , no semicolon(;) is used in any case
+getch();    //getch()= means get a character
+}
+
+
+
